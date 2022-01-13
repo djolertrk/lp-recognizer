2f6316ca4c52a7072971a75c1a432ccd529c3be6
Djordje Todorovic <djolertrk@gmail.com>
2022-01-13 15:49:10 +0100

    Adding the main driver and CMake file

---
+++ driver.cpp
@@ -0,0 +1,15 @@
+/* This is main driver of the lp-recognizer
+   tool. The tool is used to automatically
+   recognize license plates and it uses
+   the OpenALPR library.
+
+   Copyright (c) 2022 Djordje Todorovic <djolertrk@gmail.com>.
+*/
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "lp-recognizer\n";
+	return 0;
+}
