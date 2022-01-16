2dbd5105d3ad37d3e7ee223e003cb39ad3782579
Djordje Todorovic <djolertrk@gmail.com>
2022-01-16 12:47:11 +0100

    Switch to the ultimateALPR-SDK

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,7 @@
 /* This is main driver of the lp-recognizer
    tool. The tool is used to automatically
    recognize license plates and it uses
-   the OpenALPR library.
+   the https://github.com/DoubangoTelecom/ultimateALPR-SDK.
 
    Copyright (c) 2022 Djordje Todorovic <djolertrk@gmail.com>.
 */
@@ -9,32 +9,144 @@
 #include <iostream>
 #include <string>
 
-#include "alpr.h"
+#include "alpr_utils.h"
+#include "ultimateALPR-SDK-API-PUBLIC.h"
 
-int main(int argc, char const *argv[])
-{
+using namespace ultimateAlprSdk;
+
+// Configuration for ANPR deep learning engine.
+static const char* __jsonConfig =
+    "{"
+    "\"debug_level\": \"warn\","
+    ""
+    "\"gpgpu_enabled\": true,"
+    ""
+    "\"detect_roi\": [240, 1040, 60, 660],"
+    "\"detect_minscore\": 0.1,"
+    ""
+    "\"recogn_minscore\": 0.3,"
+    "\"recogn_score_type\": \"min\""
+    "";
+
+// Asset manager used on Android to files in "assets" folder.
+#if ULTALPR_SDK_OS_ANDROID
+#define ASSET_MGR_PARAM() __sdk_android_assetmgr,
+#else
+#define ASSET_MGR_PARAM()
+#endif /* ULTALPR_SDK_OS_ANDROID */
+
+static void printUsage(const std::string& message = "") {
+  if (!message.empty()) {
+    ULTALPR_SDK_PRINT_ERROR("%s", message.c_str());
+  }
+
+  ULTALPR_SDK_PRINT_INFO(
+      "\n**********************************************************************"
+      "**********\n"
+      "lp-recognizer\n"
+      "\t--image <path-to-image-with-to-recognize> \n"
+      "\t[--assets <path-to-assets-folder>] \n"
+      "************************************************************************"
+      "********\n");
+}
+
+int main(int argc, char* argv[]) {
   std::cout << "lp-recognizer\n";
 
-  // FIXME: This should be dynamic set up.
-  alpr::Alpr openalpr("us", "/etc/openalpr/openalpr.conf");
+  UltAlprSdkResult result;
 
-  if (!openalpr.isLoaded()) {
-    std::cerr << "Error with loading OpenALPR\n";
-    return 1;
+  std::string assetsFolder, licenseTokenData, licenseTokenFile;
+  bool isRectificationEnabled = false;
+  bool isCarNoPlateDetectEnabled = false;
+  bool isIENVEnabled =
+#if defined(__arm__) || defined(__thumb__) || defined(__TARGET_ARCH_ARM) || \
+    defined(__TARGET_ARCH_THUMB) || defined(_ARM) || defined(_M_ARM) ||     \
+    defined(_M_ARMT) || defined(__arm) || defined(__aarch64__)
+      false;
+#else  // x86-64
+      true;
+#endif
+  bool isOpenVinoEnabled = true;
+  bool isKlassLPCI_Enabled = false;
+  bool isKlassVCR_Enabled = false;
+  bool isKlassVMMR_Enabled = false;
+  bool isKlassVBSR_Enabled = false;
+  std::string charset = "latin";
+  std::string openvinoDevice = "CPU";
+  std::string pathFileImage;
+
+  // Parsing args.
+  std::map<std::string, std::string> args;
+  if (!alprParseArgs(argc, argv, args)) {
+    printUsage();
+    return -1;
   }
 
-  alpr::AlprResults results = openalpr.recognize("./plate-example.jpg");
+  if (args.find("--image") == args.end()) {
+    printUsage("--image required");
+    return -1;
+  }
+  pathFileImage = args["--image"];
 
-  for (int i = 0; i < results.plates.size(); i++) {
-    alpr::AlprPlateResult plate = results.plates[i];
-    std::cout << "plate" << i << ": " << plate.topNPlates.size() << " results" << std::endl;
+  if (args.find("--assets") == args.end()) {
+    printUsage("--assets required");
+    return -1;
+  }
+  assetsFolder = args["--assets"];
 
-    for (int k = 0; k < plate.topNPlates.size(); k++) {
-      alpr::AlprPlate candidate = plate.topNPlates[k];
-      std::cout << "    - " << candidate.characters << "\t confidence: " << candidate.overall_confidence;
-      std::cout << "\t pattern_match: " << candidate.matches_template << std::endl;
+  // Update JSON config
+  std::string jsonConfig = __jsonConfig;
+
+  //
+  if (!assetsFolder.empty()) {
+    jsonConfig += std::string(",\"assets_folder\": \"") + assetsFolder +
+                  std::string("\"");
+  }
+
+  if (!openvinoDevice.empty()) {
+    jsonConfig += std::string(",\"openvino_device\": \"") + openvinoDevice +
+                  std::string("\"");
+  }
+
+  // TODO: Get more parameters.
+
+  // The end of the config.
+  jsonConfig += "}";
+
+  // Decode the image.
+  AlprFile fileImage;
+  if (!alprDecodeFile(pathFileImage, fileImage)) {
+    ULTALPR_SDK_PRINT_INFO("Failed to read image file: %s",
+                           pathFileImage.c_str());
+    return -1;
+  }
+
+  // Init the recognizer.
+  ULTALPR_SDK_PRINT_INFO("Starting lp-recognizer...");
+  ULTALPR_SDK_ASSERT((result = UltAlprSdkEngine::init(
+                          ASSET_MGR_PARAM() jsonConfig.c_str(), nullptr))
+                         .isOK());
+
+  // The recognizing logic.
+  ULTALPR_SDK_ASSERT((result = UltAlprSdkEngine::process(
+                          fileImage.type, fileImage.uncompressedData,
+                          fileImage.width, fileImage.height))
+                         .isOK());
+  ULTALPR_SDK_PRINT_INFO("Processing done.");
+
+  // Print latest result.
+  if (result.json()) {
+    const std::string& json_ = result.json();
+    if (!json_.empty()) {
+      printf("%s", json_.c_str());
     }
   }
+
+  // Cleun up the lp-recognizer.
+  ULTALPR_SDK_PRINT_INFO("Ending lp-recognizer...");
+  ULTALPR_SDK_ASSERT((result = UltAlprSdkEngine::deInit()).isOK());
+
+  std::cout << "\n";
 
   return 0;
 }
